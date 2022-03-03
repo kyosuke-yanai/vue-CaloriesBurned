e7152895b015f55db00058ce1899a335c515db9c
kyosuke yanai <wyainaxio4s8s@gmail.com>
2022-03-04 00:19:32 +0900

    create toppage

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,37 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
+    <v-row>
       <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
+        class="ma-2"
+        :loading="loading"
+        :disabled="loading"
+        color="secondary"
+        @click="loader = 'loading'"
+        height="80"
+        width="160"
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+        imgが入る予定
       </v-btn>
-    </v-app-bar>
+    </v-row>
 
     <v-main>
-      <HelloWorld/>
+      <Questions />
     </v-main>
+
+    <v-footer padless fixed>
+      <v-col class="text-center" cols="12"> © KYOSUKE YANAI </v-col>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import Questions from "./components/Questions";
 
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
-    HelloWorld,
+    Questions,
   },
 
   data: () => ({
---
+++ src/components/Questions.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+    <v-container>
+      <v-row>
+        <v-col align="center">
+          <p class="text-h6">Into the Program</p>
+          <p class="text-h6">Into the Program</p>
+          <p class="text-h6">Into the Program</p>
+          <v-btn block color="success"> btn </v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "app",
+  data() {
+    return {
+      messages: "Hello vue.js!",
+    };
+  },
+};
+</script>
